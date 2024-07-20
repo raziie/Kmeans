e28e6408e2869b677bbba57df4248fdde9cd694d
Razie <rzyh1381@gmail.com>
2024-07-20 19:38:40 +0330

    add all the updated files

--- EDA.py
+++ EDA.py
@@ -1,23 +1,18 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+
+
 class EDA:
     def __init__(self, data):
         self.data = data
 
     def clean_dataset(self):
-        # print(self.data)
-        # print(self.data.shape)
-        # print(self.data.head())
-        # print(self.data.tail())
-        # print(type(self.data.info()))
-        # print(self.data.isnull().sum())
-        # print((self.data.isnull().sum()/(len(self.data)))*100)
-        # print(self.data.describe())
-
         # Remove null columns from data
         self.data.dropna(how='all', axis=1, inplace=True)
 
         # find non-numeric column names
         obj_cols = list(self.data.select_dtypes(include=['object']).columns)
-        # print(obj_cols)
 
         # check if they are useful
         for column in obj_cols:
@@ -26,7 +21,6 @@ class EDA:
             uniqueness = (unique_num / total_num) * 100
             # if most of the data is unique, then it's not useful so remove it
             if uniqueness > 90:
-                print(column)
                 self.data.drop(column, axis=1, inplace=True)
             # otherwise it's useful, so convert to numeric
             else:
@@ -35,19 +29,49 @@ class EDA:
                 unique_indexes = range(0, len(unique_vals))
                 self.data[column] = self.data[column].map(dict(zip(unique_vals, unique_indexes)))
 
+                # or
+
+                # label_encoder = LabelEncoder()
+                # self.data[column] = label_encoder.fit_transform(self.data[column])
+
         return
 
+    def remove_outliers(self):
+        for column in self.data.columns:
+            # calculate IQR for each column
+            Q1 = self.data[column].quantile(0.05)
+            Q3 = self.data[column].quantile(0.95)
+            IQR = Q3 - Q1
+
+            # identify outliers
+            threshold = 1.5
+            lower = Q1 - threshold * IQR
+            upper = Q3 + threshold * IQR
+
+            outliers = self.data[(self.data[column] < lower) | (self.data[column] > upper)]
+
+            # remove outliers
+            self.data.drop(outliers.index, inplace=True)
+
     def scale_features(self):
-        # # Min-Max Normalization
-        # self.data = (self.data - self.data.min()) / (self.data.max() - self.data.min())
+        # Min-Max Normalization
+        self.data = (self.data - self.data.min()) / (self.data.max() - self.data.min())
 
         # or
 
-        # Standardize features by removing the mean and scaling to unit variance
-        # x = (x - m) / v
-        variance = self.data.var()
-        mean = self.data.mean()
-        self.data = (self.data - mean) / variance
+        # # Standardize features by removing the mean and scaling to unit variance
+        # # x = (x - m) / v
+        # variance = self.data.var()
+        # mean = self.data.mean()
+        # self.data = (self.data - mean) / variance
+
+        # or
+
+        # column_list = self.data.columns.tolist()
+        # features = self.data[column_list]
+        # scaler = MinMaxScaler()
+        # scaled_features = scaler.fit_transform(features)
+        # self.data = pd.DataFrame(scaled_features, columns=self.data.columns)
 
         return
 
--- Main.py
+++ Main.py
@@ -1,30 +1,87 @@
-import pandas as pd
-import numpy as np
+from collections import Counter
+
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
-from sklearn.datasets import make_blobs
+from scipy.spatial.distance import cdist
+from sklearn.cluster import KMeans
 from EDA import EDA
 import warnings
 warnings.filterwarnings('ignore')
+
+
+def elbow(data):
+    # Distortion = 1/n * Σ(distance(point, centroid)^2)
+    # Inertia = Σ(distance(point, centroid)^2)
+    # Find optimum number of cluster
+    wcss = []  # SUM OF SQUARED ERROR
+    for k in range(1, 11):
+        classifier = KMeans(n_clusters=k, random_state=42)
+        classifier.fit(data)
+        wcss.append(classifier.inertia_)
+
+        # or
+
+        # wcss.append(sum(np.min(cdist(data, classifier.cluster_centers_, 'euclidean'), axis=1)) / data.shape[0])
+
+    for key, val in dict(enumerate(wcss)).items():
+        print(f'{key + 1} : {val}')
+    plot_elbow(wcss)
+
+
+def plot_elbow(wcss):
+    sns.set_style("whitegrid")
+    diagram = sns.lineplot(x=range(1, 11), y=wcss)
+    diagram.set(xlabel="Number of clusters (k)", ylabel="Within-Cluster Sum of Square", title='Elbow Method')
+    plt.show()
+
 
 if __name__ == "__main__":
     dataset = pd.read_csv('./facebook/Live.csv')
     print(dataset)
     print(dataset.info())
+    print(dataset.columns)
+    print(dataset.shape)
+    print(dataset.head())
+    print(dataset.tail())
+    print(dataset.isnull().sum())
+    print((dataset.isnull().sum()/(len(dataset)))*100)
+    print(dataset.describe())
 
     data_EDA = EDA(dataset)
     data_EDA.clean_dataset()
     print(data_EDA.get_data())
     print(data_EDA.get_data().info())
 
+    # data_EDA.remove_outliers()
+    # print(data_EDA.get_data())
+    # print(data_EDA.get_data().info())
+
     data_EDA.scale_features()
     print(data_EDA.get_data())
 
     # apply kmeans clustering
+    elbow(data_EDA.get_data())
 
-    # X, y = make_blobs(n_samples=500, n_features=2, centers=3, random_state=23)
-    #
-    # fig = plt.figure(0)
-    # plt.grid(True)
-    # plt.scatter(X[:, 0], X[:, 1])
-    # plt.show()
+    kmeans = KMeans(n_clusters=4, init="k-means++", random_state=42)
+    kmeans.fit(data_EDA.get_data())
+    print(kmeans.cluster_centers_)
+    print(kmeans.inertia_)
+
+    prediction = kmeans.fit_predict(data_EDA.get_data())
+    print(prediction)
+    print(*Counter(prediction))
+
+    # plot clusters for features 4,5
+    X = data_EDA.get_data().iloc[:, [4, 5]].values
+    plt.scatter(X[prediction == 0, 0], X[prediction == 0, 1], s=60, c='red', label='Cluster1')
+    plt.scatter(X[prediction == 1, 0], X[prediction == 1, 1], s=60, c='blue', label='Cluster2')
+    plt.scatter(X[prediction == 2, 0], X[prediction == 2, 1], s=60, c='green', label='Cluster3')
+    plt.scatter(X[prediction == 3, 0], X[prediction == 3, 1], s=60, c='violet', label='Cluster4')
+    # plt.scatter(X[prediction == 4, 0], X[prediction == 4, 1], s=60, c='yellow', label='Cluster5')
+    plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=100, c='black', label='Centroids')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend()
+
+    plt.show()
